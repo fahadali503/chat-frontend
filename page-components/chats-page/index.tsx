3613e8fe747934f91857f656e76645a77b86cce6
Fahad Ali <fahada094@gmail.com>
2023-05-14 07:25:05 +0500

    exception handling

--- page-components/chats-page/index.tsx
+++ page-components/chats-page/index.tsx
@@ -1,4 +1,4 @@
-import { Grid } from "@mantine/core";
+import { Grid, Notification } from "@mantine/core";
 import { ChatListComponent } from "./chat-list/ChatList";
 import { SelectedChat } from "./chat";
 import { useEffect, useState } from "react";
@@ -7,6 +7,8 @@ import { IChat } from "@/types/chat.types";
 import { IUser } from "@/types/user.types";
 import { useAuthenticatedSocket } from "@/hooks/useAuthenticatedSocket";
 import { useSocket, useSocketEvent } from "socket.io-react-hook";
+import { IconCheck, IconX } from "@tabler/icons-react";
+import { notifications } from "@mantine/notifications";
 
 const chatApi = new ChatApi();
 
@@ -15,6 +17,29 @@ export function ChatsPageComponent() {
     const [selectedChatId, setSelectedChatId] = useState("")
     const [selectedChat, setSelectedChat] = useState<IChat | null>(null);
     const [friend, setFriend] = useState<IUser | null>(null);
+    const { socket, connected, error } = useAuthenticatedSocket();
+
+    useEffect(() => {
+        if (connected) {
+            console.log("Socket io connected", connected);
+            notifications.show({
+                message: "Connection established to the server",
+                icon: <IconCheck size="1.1rem" />,
+                color: "green"
+            })
+        }
+        socket.on('exception', (error) => {
+            notifications.show({
+                message: error.message,
+                icon: <IconX size="1.1rem" />,
+                color: "red"
+            })
+        });
+        return () => {
+            socket.off("test");
+            socket.off("exception");
+        }
+    }, [connected])
 
     useEffect(() => {
         if (selectedFriendFromSearch.length > 0) {
